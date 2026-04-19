1ad68a1324a531108ec419b25a1d1fe176ccab5e
ShalamanShalaman <YOUR_GITHUB_EMAIL@example.com>
2026-04-19 10:59:54 +0800

    sync'd changes from laravel_deploy to eaas_laravel for local testing

--- eaas_frontend/src/components/Header.jsx
+++ eaas_frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const NotificationDropdown = () => null;
 const MessageDropdown = () => null;
 
-const API_BASE_URL = "";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function Header({ role, setRole, user, onLogout }) {
   const canToggleRoles = user && (user.role === 'admin' || user.role === 'hr');
