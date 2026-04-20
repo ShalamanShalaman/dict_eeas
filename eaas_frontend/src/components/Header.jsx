cec81f3706115765a6f55c01f8502279222a3d3d
ShalamanShalaman <YOUR_GITHUB_EMAIL@example.com>
2026-04-20 08:47:03 +0800

    Revert "sync'd changes from laravel_deploy to eaas_laravel for local testing"
    This reverts commit 1ad68a1324a531108ec419b25a1d1fe176ccab5e.

--- eaas_frontend/src/components/Header.jsx
+++ eaas_frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import NotificationDropdown from "./NotificationDropdown";
 import MessageDropdown from "./MessageDropdown";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "";
 
 export default function Header({ role, setRole, user, onLogout }) {
   const canToggleRoles = user && (user.role === 'admin' || user.role === 'hr');
